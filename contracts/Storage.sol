48a3cd14c402579360d3c021095e6846201e1799
Neel Patel <neel199907@gmail.com>
2022-06-18 14:14:24 +0530

    added truffle project and storage smart contract

---
+++ contracts/Storage.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Storage {
+    string public name = "Storage Chain";
+    uint256 public fileCount = 0;
+    mapping(uint256 => File) public files;
+
+    struct File {
+        uint256 fileId;
+        string fileHash;
+        uint256 fileSize;
+        string fileType;
+        string fileName;
+        string fileDescription;
+        uint256 uploadTime;
+        address payable uploader;
+    }
+    event FileUploaded(
+        uint256 fileId,
+        string fileHash,
+        uint256 fileSize,
+        string fileType,
+        string fileName,
+        string fileDescription,
+        uint256 uploadTime,
+        address payable uploader
+    );
+
+    constructor() public {}
+
+    function uploadFile(
+        string memory _fileHash,
+        uint256 _fileSize,
+        string memory _fileType,
+        string memory _fileName,
+        string memory _fileDescription
+    ) public {
+        // all basic requirement conditions to check
+        require(bytes(_fileHash).length > 0);
+        require(bytes(_fileType).length > 0);
+        require(bytes(_fileName).length > 0);
+        require(bytes(_fileDescription).length > 0);
+        require(msg.sender != address(0));
+        require(_fileSize > 0);
+
+        // increment file id
+        fileCount++;
+
+        // Add File to the contract
+        files[fileCount] = File(
+            fileCount,
+            _fileHash,
+            _fileSize,
+            _fileType,
+            _fileName,
+            _fileDescription,
+            now,
+            msg.sender
+        );
+
+        // trigger an event
+        emit FileUploaded(
+            fileCount,
+            _fileHash,
+            _fileSize,
+            _fileType,
+            _fileName,
+            _fileDescription,
+            now,
+            msg.sender
+        );
+    }
+}
